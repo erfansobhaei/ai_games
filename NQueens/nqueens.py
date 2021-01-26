658e1ae4b8d5f9181a0254ee7ba66e88849e06e7
Erfan Sobhaei <e.sobhaei@gmail.com>
2021-01-26 21:48:54 +0330

    Function for calculating not checking pair queens created

--- NQueens/nqueens.py
+++ NQueens/nqueens.py
@@ -16,4 +16,32 @@ def init(src):
         queens = [[int(item), index] for index,item in enumerate(tmp)]
 
 
+def get_evalfunc_table():
+    evalfunc_values = [ [0 for _ in range(n)] for _ in range(n) ]
+    for i in range(n):
+        for j in range(n):
+            tmp = copy.deepcopy(queens)
+            tmp[j] = [i, j]
+            evalfunc_values[i][j] = calculate_h(tmp)
+    return evalfunc_values
+
+
+def calculate_h(queens):
+    checks = 0
+    for i in range(n):
+        for j in range(i+1,n):
+            if queens[i][0] == queens[j][0]:
+                checks += 1
+                continue
+            delta_y = abs(queens[i][0]-queens[j][0])
+            delta_x = abs(queens[i][1]-queens[j][1])
+            if delta_x == delta_y:
+                checks += 1
+
+    return int((n*(n-1))/2) - (checks)
+
+
 init(sys.argv[1])
+table = get_evalfunc_table()
+
+
